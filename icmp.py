90c711c781602de7267b66ad58a482afb4538c78
Edmond <pefouho-edmond.nguefeu>
2023-11-17 14:52:35 +0100

    icmp using scapy

--- icmp.py
+++ icmp.py
@@ -1,5 +1,7 @@
 import subprocess
 from colorama import Fore, Back, Style
+from scapy.all import *
+from scapy.layers.inet import ICMP, IP
 
 
 
@@ -7,12 +9,18 @@ def ic(ip_list):
     toprint = ""
 
     for address in ip_list['ip']:
-        print("\n\n SCANNING... Please Wait \n\n")
+        '''print("\n\n SCANNING... Please Wait \n\n")
         res = subprocess.call(['ping', '-c', '3', address])
         if res == 0:
             toprint += f" {address} {Fore.GREEN}is LIVE\n{Style.RESET_ALL}"
         else:
-            toprint += f" {address} {Fore.RED}is NOT LIVE\n{Style.RESET_ALL}"
+            toprint += f" {address} {Fore.RED}is NOT LIVE\n{Style.RESET_ALL}"'''
+        reponse, b = sr(IP(dst=str(address))/ICMP(), timeout=3, verbose=1)
+        if reponse == False:
+                toprint += f" {address} {Fore.GREEN}is LIVE\n{Style.RESET_ALL}"
+
+        else:
+                toprint += f" {address} {Fore.RED}is NOT LIVE\n{Style.RESET_ALL}"
 
 
     print("\n\n\n")
