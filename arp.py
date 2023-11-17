26c8df89b073fb6e45b1641bbc680d70c6330dcd
jonathan-sa <jsa@desktop-jsa.home>
2023-11-17 11:29:36 +0100

    model

--- arp.py
+++ arp.py
@@ -2,41 +2,29 @@ import sys
 from scapy.all import *
 import requests
 import time
-
+from scapy.layers.l2 import ARP, Ether, srp
 
 
 
 def art(n):
     print("\n*************** ARP SCAN ***************\n")
-    
 
-    request = scapy.ARP()
-  
+    for add in n['ip']:
+        request = ARP()
+        request.pdst = add
 
-    request.pdst = n
+        broadcast = Ether()
+        broadcast.dst = 'ff:ff:ff:ff:ff:ff'
 
+        request_broadcast = broadcast / request
+        clients = srp(request_broadcast, timeout=1, verbose=False)[0]
 
-    broadcast = scapy.Ether()
-  
-    broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-  
-    
-    request_broadcast = broadcast / request
-    clients = scapy.srp(request_broadcast, timeout = 1, verbose = False)[0]
-    
-    url = "https://api.macvendors.com/"
-    #Print IP and MAC addresses
-    for i in clients:
-        #We delay the execution of our function so the API doesn't display us an error message
-        print("\n")
-        time.sleep(1)
-        print(i[1].psrc + "      " + i[1].hwsrc)
-        #We use the API of MAC Vendors to get the manufactures name
+        url = "https://api.macvendors.com/"
+        for i in clients:
+            print("\n")
+            time.sleep(1)
+            print(i[1].psrc + "      " + i[1].hwsrc)
 
-        #We use get method to fetch details of the MAC address
-        
-            
-        response = requests.get(url+ i[1].hwsrc)
-        r = response.content.decode()
-        print(response.content.decode())
-        
+            response = requests.get(url + i[1].hwsrc)
+            r = response.content.decode()
+            print(response.content.decode())
